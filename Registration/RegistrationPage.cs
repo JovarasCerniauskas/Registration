eb8bf2f0189870a767c6b5fb3bef6ea3c53b0297
DESKTOP-O85E253\Jovaras <jovaras@viacorex.com>
2023-05-31 11:35:53 +0300

    Add project files.

---
+++ Registration/RegistrationPage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace Registration
+{
+    internal class RegistrationPage
+    {
+        private IWebDriver _driver;
+
+        [FindsBy(How = How.Id, Using = "input-firstname")]
+        private IWebElement firstnameTextbox;
+
+        [FindsBy(How = How.Id, Using = "input-lastname")]
+        private IWebElement lastnameTextbox;
+
+        [FindsBy(How = How.Id, Using = "input-email")]
+        private IWebElement emailTextbox;
+
+        [FindsBy(How = How.Id, Using = "input-telephone")]
+        private IWebElement telephoneTextbox;
+
+        [FindsBy(How = How.Id, Using = "input-password")]
+        private IWebElement passwordTextbox;
+
+        [FindsBy(How = How.Id, Using = "input-confirm")]
+        private IWebElement confirmPasswordTextbox;
+
+        [FindsBy(How = How.Name, Using = "agree")]
+        private IWebElement privacyPolicyCheckbox;
+
+        [FindsBy(How = How.XPath, Using = "//input[@value='Continue']")]
+        private IWebElement continueButton;
+
+        public RegistrationPage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void Register(string firstName, string lastName, string email, string telephoneNumber, string password, string confirmPassword)
+        {
+            firstnameTextbox.SendKeys(firstName);
+            lastnameTextbox.SendKeys(lastName);
+            emailTextbox.SendKeys(email);
+            telephoneTextbox.SendKeys(telephoneNumber);
+            passwordTextbox.SendKeys(password);
+            confirmPasswordTextbox.SendKeys(confirmPassword);
+            privacyPolicyCheckbox.Click();
+            continueButton.Click();
+
+        }
+    }
+}
---
+++ Registration/RegistrationTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Registration
+{
+    internal class RegistrationTest
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+          
+        public void RegisterAccount()
+        {
+            RegistrationPage regPage = new RegistrationPage(driver);
+            driver.Navigate().GoToUrl("https://op.akademijait.vtmc.lt/index.php?route=account/register");
+            regPage.Register("karolis", "klizas", "karolis.klizas4@mailinator.com", "+37000000", "Password123!", "Password123!");
+            Assert.True(driver.PageSource.Contains("Your Account Has Been Created!"));
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
